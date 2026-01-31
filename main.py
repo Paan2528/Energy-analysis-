6fca3643002bd87bf1770a2f4c9d60f606b6acfe
Paan2528 <janjira8211@gmail.com>
2026-01-31 10:52:49 +0100

    move analysis from main to analysis.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from src.analysis import find_below_average_days
+from src.visualization import plot_daily_energy
+from src.analysis import calculate_daily_energy
+from src.data_cleaning import clean_data
 import pandas as pd
 import matplotlib.pyplot as plt
 # Load data
 from src.data_loader import load_data
-df = load_data('/Users/yatoum/Documents/Solar-energy-project/data/raw/Plant_1_Generation_Data.csv')
+df = load_data(
+    '/Users/yatoum/Documents/Solar-energy-project/data/raw/Plant_1_Generation_Data.csv')
 
 # clear NaN values
-from src.data_cleaning import clean_data
 df = clean_data(df)
 
 # change string to number
@@ -14,16 +18,14 @@ df['DC_POWER'] = pd.to_numeric(df['DC_POWER'], errors='coerce')
 
 # convert 'DATE_TIME' column to datetime format
 df['DATE_TIME'] = pd.to_datetime(df['DATE_TIME'], dayfirst=True)
-#only kepp day
+# only kepp day
 df['day'] = df['DATE_TIME'].dt.date
 
-# resample data to daily frequency, summing up 'DC_POWER' valuesit 
+# resample data to daily frequency, summing up 'DC_POWER' values
 
-from src.analysis import calculate_daily_energy
 daily = calculate_daily_energy(df)
 
-'graph daily data'
-from src.visualization import plot_daily_energy
+# graph daily data
 plot_daily_energy(daily)
 
 print("daily:")
@@ -31,9 +33,8 @@ print(daily.describe())
 
 # ====== Analysis =======
 
-average_energy = daily.mean()
-print("\nAverage daily energy generation:", average_energy)
+avg, below_avg_days = find_below_average_days(daily)
+
+print("\nAverage daily energy generation:", avg)
 print("\nDays with below-average energy:")
-for day, value in daily.items(): 
-    if value < average_energy:
-        print(day, "->", value) 
+print(below_avg_days)
