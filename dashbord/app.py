eb91a9a770ab942efb146a24ffec7df8a99fded9
Paan2528 <140539606+Paan2528@users.noreply.github.com>
2026-03-11 18:57:26 +0100

    Docker

--- dashbord/app.py
+++ dashbord/app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import requests
 import pandas as pd
+import os
 
-API = "http://127.0.0.1:8002"
+API = os.getenv("API_URL", "http://127.0.0.1:8002")
 
 st.title("Solar Energy Dashboard (project 4)")
 
